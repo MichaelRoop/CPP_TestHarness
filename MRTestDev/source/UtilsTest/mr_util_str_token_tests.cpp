b54dcbd3efd0be30593b962ef76962aa53669ab0
Michael Roop <michael@roop.ca>
2012-08-12 20:20:00 -0300

    Cleaned out commented old format test case

--- MRTestDev/source/UtilsTest/mr_util_str_token_tests.cpp
+++ MRTestDev/source/UtilsTest/mr_util_str_token_tests.cpp
@@ -13,13 +13,21 @@ public:
 
 	TokenizerTests1() 
 		: mr_test::testCase( _L_("testName"), _L_("argStr") ) {
+
+		// This will add the test fixture to the test engine list when it is constructed
+		// The Register Fixture macro at the end of the class creates an instance which
+		// fires the contructor. However, this only works if the test case is compiled in
+		// to a test app.  TODO - secondary step - register contructor with a method
+		// that could be called from a DLL when the engine loads that DLL with the tests
 		_ADD_TEST_FIXTURE_( this );
 
+		// Register setup and teardown methods
 		_FIXTURE_SETUP_(this, &TokenizerTests1::FixtureSetup );
 		_FIXTURE_TEARDOWN_(this, &TokenizerTests1::FixtureTeardown );
 		_TEST_SETUP_(this, &TokenizerTests1::TestSetup);
 		_TEST_TEARDOWN_(this, &TokenizerTests1::TestTeardown);
 
+		// Register test case methods
 		_REGISTER_TEST_(this, &TokenizerTests1::UTL_TOK_1_1, _L_( "Normal tokenize string" ));
 		_REGISTER_TEST_(this, &TokenizerTests1::UTL_TOK_1_2, _L_( "Tokenize with multiple mid delimiters"));
 		_REGISTER_TEST_(this, &TokenizerTests1::UTL_TOK_1_3, _L_( "Tokenize test leading delimiters" ));
@@ -83,9 +91,7 @@ public:
 		mr_utils::mr_string str( _L_("||||||||This|is|a|test||||||string|with||||||some|tokens||||||||") );
 		this->DoIt( str );
 	}
-
-
-
+	
 protected:
 
 	std::vector<mr_utils::mr_string> m_tokens;
@@ -133,145 +139,6 @@ protected:
 };
 _REGISTER_FIXTURE_(TokenizerTests1)
 
-//
-//class blah : public TokTestBase {
-//public:
-//	blah() : TokTestBase( _L_( "blah" ), _L_( "Blah class" ) )	{
-//		_ADD_TEST_FIXTURE_( this );
-//		_REGISTER_TEST_(this, &blah::UTL_TOK_1_1, _L_( "Normal tokenize string" ));
-//		_REGISTER_TEST_(this, &blah::UTL_TOK_1_2, _L_( "Tokenize with multiple mid delimiters"));
-//		_REGISTER_TEST_(this, &blah::UTL_TOK_1_3, _L_( "Tokenize test leading delimiters" ));
-//		_REGISTER_TEST_(this, &blah::UTL_TOK_1_4, _L_( "Tokenize test following delimiters" ));
-//		_REGISTER_TEST_(this, &blah::UTL_TOK_1_5, _L_("Tokenize test leading and following delimiters"));
-//	}
-//
-//	// just to satisfy compiler with old code
-//	bool test()	{ return false;}
-//
-//	void UTL_TOK_1_1() { 
-//		mr_utils::mr_string str( _L_("This|is|a|test|string|with|some|tokens") );
-//		this->DoIt( str );
-//	}
-//
-//	void UTL_TOK_1_2() { 
-//		mr_utils::mr_string str( _L_("This|is|a|test|string|with|some|||||tokens") );
-//		this->DoIt( str );
-//	}
-//	
-//	void UTL_TOK_1_3() { 
-//		mr_utils::mr_string str( _L_("||||||||This|is|a|test|string|with|some|tokens") );
-//		this->DoIt( str );
-//	}
-//
-//	void UTL_TOK_1_4() { 
-//		mr_utils::mr_string str( _L_("This|is|a|test|string|with|some|tokens||||||||") );
-//		this->DoIt( str );
-//	}
-//	
-//	void UTL_TOK_1_5() { 
-//		mr_utils::mr_string str( _L_("||||||||This|is|a|test|string|with|some|tokens||||||||") );
-//		this->DoIt( str );
-//	}
-//
-//};
-//_REGISTER_FIXTURE_(blah);
-
-#ifdef erotiuertiou
-
-// Test tokenize of string with multiple token delimiters.
-class mrTokTestNormal : public TokTestBase {
-public:
-	mrTokTestNormal() : TokTestBase( _L_( "UTL_TOK_1_1" ), _L_( "Normal tokenize string" ) )	{
-		_ADD_TEST_FIXTURE_( this );
-	}
-
-	bool test()	{ 
-		mr_utils::mr_string str( _L_("This|is|a|test|string|with|some|tokens") );
-		return this->DoIt( str );
-	}
-};
-_REGISTER_FIXTURE_(mrTokTestNormal);
-
-
-// Test tokenize of string with multiple token delimiters and mid extras.
-class mrTokTestMidMultipleDel : public TokTestBase {
-public:
-	mrTokTestMidMultipleDel() : TokTestBase(_L_("UTL_TOK_1_2"), _L_( "Tokenize with multiple mid delimiters")) {
-		_ADD_TEST_FIXTURE_( this );
-	}
-
-	bool test()	{ 
-		mr_utils::mr_string str( _L_("This|is|a|test|string|with|some|||||tokens") );
-		return this->DoIt( str );
-	}
-};
-_REGISTER_FIXTURE_(mrTokTestMidMultipleDel);
-
-
-// Test tokenize of string with leading token delimiters.
-class mrTokTestLeadDel : public TokTestBase {
-public:
-	mrTokTestLeadDel() : TokTestBase(_L_("UTL_TOK_1_3"), _L_( "Tokenize test leading delimiters" )) {
-		_ADD_TEST_FIXTURE_( this );
-	}
-
-	bool test()		
-	{ 
-		mr_utils::mr_string str( _L_("||||||||This|is|a|test|string|with|some|tokens") );
-		return this->DoIt( str );
-	}
-};
-_REGISTER_FIXTURE_(mrTokTestLeadDel);
-
-
-// Test tokenize of string with following token delimiters.
-class mrTokTestFollowDel : public TokTestBase {
-public:
-	mrTokTestFollowDel() : TokTestBase(_L_("UTL_TOK_1_4"), _L_( "Tokenize test following delimiters" )) {
-		_ADD_TEST_FIXTURE_( this );
-	}
-
-	bool test()		
-	{ 
-		mr_utils::mr_string str( _L_("This|is|a|test|string|with|some|tokens||||||||") );
-		return this->DoIt( str );
-	}
-};
-_REGISTER_FIXTURE_(mrTokTestFollowDel);
-
-
-// Test tokenize of string with leading and following token delimiters.
-class mrTokTestLeadFollowDel : public TokTestBase {
-public:
-	mrTokTestLeadFollowDel() : TokTestBase(_L_("UTL_TOK_1_5"), _L_( "Tokenize test leading and following delimiters" )) {
-		_ADD_TEST_FIXTURE_( this );
-	}
-							 
-	bool test()		
-	{ 
-		mr_utils::mr_string str( _L_("||||||||This|is|a|test|string|with|some|tokens||||||||") );
-		return this->DoIt( str );
-	}
-};
-_REGISTER_FIXTURE_(mrTokTestLeadFollowDel);
-
-#endif
-
-//// Test tokenize of string with multiple mid and leading and following token delimiters.
-//class mrTokTestLeadMidFollowDel : public TokTestBase {
-//public:
-//	mrTokTestLeadMidFollowDel()
-//		: TokTestBase( L( "UTL_TOK_1_6" ), _L_( "Tokenize test multiple leading mid and following delimiters" ) )	{
-//		_ADD_TEST_FIXTURE_( this );
-//	}
-//
-//	bool test()	{ 
-//		mr_utils::mr_string str( _L_("||||||||This|is|a|test||||||string|with||||||some|tokens||||||||") );
-//		return this->DoIt( str );
-//	}
-//};
-//_REGISTER_FIXTURE_(mrTokTestLeadMidFollowDel);
-
 
 // Test tokenize of string with only token delimiters.
 class TokenizerTests2 : public mr_test::testCase {
@@ -312,8 +179,6 @@ public:
 	void FixtureTeardown() {
 		printf("++++ 2 Registered Fixture Teardown Executed ++++\n");
 	}
-
-
 
 	void UTL_TOK_1_7() { 
 		mr_utils::mr_string str( _L_("||||||||") );
