62d53e5101120c234d1eac5e516384fe382b316f
Michael Roop <michael@roop.ca>
2012-08-27 20:16:47 -0300

    Cleanup DLL main

--- CppTestUtilsTestCases/dllmain.cpp
+++ CppTestUtilsTestCases/dllmain.cpp
@@ -1,25 +1,15 @@
 // dllmain.cpp : Defines the entry point for the DLL application.
 
 #if defined(WIN32)
-//#define WIN32_LEAN_AND_MEAN             // Exclude rarely-used stuff from Windows headers
-//// Windows Header Files:
-#include <windows.h>
-
-//#include <WinBase.h>
+#	define WIN32_LEAN_AND_MEAN // Exclude rarely-used stuff from Windows headers
+#	include <windows.h>
 #endif
 
 #include <cstdio>
 
 #include "mr_util_str_token_tests.cpp"
 
-BOOL APIENTRY DllMain( HMODULE hModule,
-                       DWORD  ul_reason_for_call,
-                       LPVOID lpReserved
-					 )
-{
-
-	printf("-----------------------------Utils Test Cases DllMain Invoked\n");
-
+BOOL APIENTRY DllMain(HMODULE hModule, DWORD  ul_reason_for_call, LPVOID lpReserved ) {
 	switch (ul_reason_for_call)
 	{
 	case DLL_PROCESS_ATTACH:
@@ -27,14 +17,11 @@ BOOL APIENTRY DllMain( HMODULE hModule,
 		{
 			_REGISTER_FIXTURE_(TokenizerTests1)
 			_REGISTER_FIXTURE_(TokenizerTests2)
-
 		}
 		break;
 	case DLL_THREAD_ATTACH:
-		printf("-----------------------------DLL_THREAD_ATTACH - Utils Test Cases\n");
 		break;
 	case DLL_THREAD_DETACH:
-		printf("-----------------------------DLL_THREAD_ATTACH - Utils Test Cases\n");
 		break;
 	case DLL_PROCESS_DETACH:
 		printf("-----------------------------DLL_PROCESS_DETACH - Utils Test Cases\n");
